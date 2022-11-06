0b72e4f375182c3fb2258d83d4cb19796729d18e
Osama Zaman <osamazaman@outlook.com>
2022-11-07 00:06:44 +0500

    refactored

--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SignIn } from "./components/SignIn";
-import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3001", () => {
-  console.log("connected to server", socket.id);
-});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<SignIn socket={socket} />} />
-      <Route path="/chat" element={<App socket={socket} />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
